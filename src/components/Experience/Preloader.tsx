0df84b9b9e29e8f6c1100df9029212924a9a0aa9
Zen <caiyuzhen00@gmail.com>
2022-12-26 00:56:00 +0800

    intro animation

--- src/components/Experience/Preloader.tsx
+++ src/components/Experience/Preloader.tsx
@@ -22,6 +22,8 @@ export default class Preloader extends EventEmitter {
 	public world: World
 	public room!: Scene //因为 Room 内的物体是挂载到 scene 上的 -> this.actualRoom = this.room.scene 
 	public timeline!: gsap.core.Timeline
+	public secondTimeline!: gsap.core.Timeline
+	public scrollOnceEvent!: (e: WheelEvent) => void //是一个函数, 用来绑定 e 的指向
 
 
 	constructor() {
@@ -56,32 +58,64 @@ export default class Preloader extends EventEmitter {
 	}
 
 
-	// loading 加载动画
+
+	// loading 加载动画 _ 位移小方块
 	firstIntro() {
-		this.timeline = new GSAP.core.Timeline()
+		// 🚀🚀🚀 用异步的方式来触发这个动画, 不然一开始就会被滚动条的事件给触发了
+		return new Promise((resolve) => {
+			this.timeline = new GSAP.core.Timeline()
+
+			// 根据不同的设备来展示不同的动画
+			if(this.device === 'desktop') {
+				this.timeline.to(this.roomChildren.cube.scale, { //把所有子元素统一缩放
+					x: 1.4,
+					y: 1.4,
+					z: 1.4,
+					ease: "back.out(2.5)",  //小盒子放大又缓动缩小的效果
+					duration: 1,
+				}).to(this.room.position, {
+					x: -1, //向左移动
+					ease: "power1.out", //🌟参考 https://greensock.com/docs/v3/Eases
+					duration: 1,
+					onComplete: resolve,
+				})
+			} else if (this.device === 'mobile') { 
+				this.timeline.to(this.roomChildren.cube.scale, { //把所有子元素统一缩放
+					x: 1.4,
+					y: 1.4,
+					z: 1.4,
+					ease: "back.out(2.5)",  //小盒子放大又缓动缩小的效果
+					duration: 1,
+				}).to(this.room.position, {
+					z: -1, //向上移动
+					ease: "power1.out", //🌟参考 https://greensock.com/docs/v3/Eases
+					duration: 1,
+					onComplete: resolve,
+				})
+			}
+		})
+	}
+
+
+
+	// loading 加载动画 _ 房间变大
+	secondIntro() {
+		this.secondTimeline = new GSAP.core.Timeline()
 
 		// 根据不同的设备来展示不同的动画
 		if(this.device === 'desktop') {
-			this.timeline.to(this.roomChildren.cube.scale, { //把所有子元素统一缩放
-				x: 1.4,
-				y: 1.4,
-				z: 1.4,
-				ease: "back.out(2.5)",  //小盒子放大又缓动缩小的效果
-				duration: 1,
-			}).to(this.room.position, {
-				x: -1,
+			this.secondTimeline.to(this.room.position, {
+				x: 0,
+				y: 0,
+				z: 0, 
 				ease: "power1.out", //🌟参考 https://greensock.com/docs/v3/Eases
 				duration: 1,
 			})
 		} else if(this.device === 'mobile') { 
-			this.timeline.to(this.roomChildren.cube.scale, { //把所有子元素统一缩放
-				x: 1.4,
-				y: 1.4,
-				z: 1.4,
-				ease: "back.out(2.5)",  //小盒子放大又缓动缩小的效果
-				duration: 1,
-			}).to(this.room.position, {
-				z: -1, //向上移动
+			this.secondTimeline.to(this.room.position, {
+				x: 0,
+				y: 0,
+				z: 0, 
 				ease: "power1.out", //🌟参考 https://greensock.com/docs/v3/Eases
 				duration: 1,
 			})
@@ -89,8 +123,32 @@ export default class Preloader extends EventEmitter {
 	}
 
 
-	// 最终执行动画
-	playIntro() {
-		this.firstIntro()
+
+	//判断鼠标滚动的方向, 然后开始播放动画, WheelEvent 对象才有 deltaY 属性
+	onScroll(e: WheelEvent) {
+		if(e.deltaY > 0) { //鼠标向下滚动
+			console.log('向下滚动了')
+			// console.log(this.room.position)  // x = -1
+			window.removeEventListener('wheel', this.scrollOnceEvent) //值播放一次，所以要移除监听事件
+			this.playSecondIntro()
+		}
+	}
+
+
+
+	// 最终执行动画 _ 位移小方块的动画 (🚀🚀🚀异步, 等动画播放完毕后再执行下面的代码, 才回去监听鼠标滚动事件)
+	async playIntro() {
+		await this.firstIntro()
+
+		// 监听鼠标滚动事件, 向下滚动时则播放一则动画
+		this.scrollOnceEvent = this.onScroll.bind(this) //🔥🔥 把 e 绑定给 onScroll 函数
+		window.addEventListener('wheel', this.scrollOnceEvent)  
+	}
+
+
+
+	// 最终执行动画 _ 房间变大的动画
+	playSecondIntro() {
+		this.secondIntro() 
 	}
 }
